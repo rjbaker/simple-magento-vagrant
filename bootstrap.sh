af96cc7fed3ef9e5bb78bd295010eb52e8e757c2
cookieflow <rbaker86@gmail.com>
2013-07-26 11:31:12 +0100

    Update bootstrap.sh
    Changed hostname from localhost to IP so cookies work
    Change to web directory before attempting installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -50,7 +50,7 @@ mysql -u root -e "FLUSH PRIVILEGES"
 # --------------------
 # http://www.magentocommerce.com/wiki/1_-_installation_and_configuration/installing_magento_via_shell_ssh
 
-# Download and 
+# Download and extract
 if [ ! -f "/vagrant/httpdocs/index.php" ]; then
   cd /vagrant/httpdocs
   wget http://www.magentocommerce.com/downloads/assets/1.7.0.2/magento-1.7.0.2.tar.gz
@@ -64,11 +64,12 @@ fi
 
 # Run installer
 if [ ! -f "/vagrant/httpdocs/app/etc/local.xml" ]; then
+  cd /vagrant/httpdocs
   sudo /usr/bin/php -f install.php -- --license_agreement_accepted yes \
   --locale en_US --timezone "America/Los_Angeles" --default_currency USD \
   --db_host localhost --db_name magentodb --db_user magentouser --db_pass password \
-  --url "http://localhost:8080/" --use_rewrites yes \
-  --use_secure no --secure_base_url "http://localhost:8080/" --use_secure_admin no \
+  --url "http://127.0.0.1:8080/" --use_rewrites yes \
+  --use_secure no --secure_base_url "http://127.0.0.1:8080/" --use_secure_admin no \
   --skip_url_validation yes \
   --admin_lastname Owner --admin_firstname Store --admin_email "admin@example.com" \
   --admin_username admin --admin_password password123123
