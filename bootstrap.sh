356428ef2a76032e148535d728f13b7b057cc2de
Richard Baker <rbaker86@gmail.com>
2014-12-26 09:17:35 +0000

    Added curl extension

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ DATA_VERSION="1.9.0.0"
 apt-get install -y apache2
 apt-get install -y php5
 apt-get install -y libapache2-mod-php5
-apt-get install -y php5-mysqlnd php5-xdebug php5-gd php5-intl php-pear php5-imap php5-mcrypt php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php-soap
+apt-get install -y php5-mysqlnd php5-curl php5-xdebug php5-gd php5-intl php-pear php5-imap php5-mcrypt php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php-soap
 
 php5enmod mcrypt
 
