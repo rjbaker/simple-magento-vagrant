191b5695086b445b81b99cd84930c6b13ab18f3e
Johannes Klein <jo@temka.de>
2015-01-14 15:25:46 +0100

    Enable "Allow Symlinks" for modman
    (found under System > Configuration > Advanced > Developer) via n98-magerun

--- bootstrap.sh
+++ bootstrap.sh
@@ -112,6 +112,7 @@ cd /vagrant/httpdocs
 wget https://raw.github.com/netz98/n98-magerun/master/n98-magerun.phar
 chmod +x ./n98-magerun.phar
 sudo mv ./n98-magerun.phar /usr/local/bin/
+n98-magerun.phar dev:symlinks --on --global
 
 # Install modman
 # --------------------
